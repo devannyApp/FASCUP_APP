6051b95c21872d0090f503caeb4542eccf86d189
devannyApp <devannyApp@gmail.com>
2018-03-14 17:13:25 -0500

    avances formularios 14 de marzo del 2018

---
+++ app/src/main/java/android/fascup/com/fascup/FormIIIActivity.java
@@ -0,0 +1,32 @@
+package android.fascup.com.fascup;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by GIOVANNY on 13/03/2018.
+ */
+
+public class FormIIIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_iii);
+
+        //Muestra el boton para volver
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Cierra la actividad al presionar el boton
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==android.R.id.home){
+            finish();
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/android/fascup/com/fascup/MainActivity.java
+++ app/src/main/java/android/fascup/com/fascup/MainActivity.java
@@ -6,29 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private CardView CardViewForm_I, CardViewForm_II, CardViewForm_III, CardViewForm_IV;   //Declaraciones de varibales para botones
 
-     Button BtnFormI;
+    //Varibles del menu
+    private CardView CardViewForm_I, CardViewForm_II, CardViewForm_III;   //Declaraciones de varibales para botones
+    Button BtnFormI;
+
+
+    //METODO
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //Definición de Cards
         CardViewForm_I = (CardView) findViewById(R.id.CardViewForm_I);
         CardViewForm_II = (CardView) findViewById(R.id.CardViewForm_II);
         CardViewForm_III = (CardView) findViewById(R.id.CardViewForm_III);
-        CardViewForm_IV = (CardView) findViewById(R.id.CardViewForm_IV);
+
 
         //Agregar Click listenrer a los CardsView de Formulario
         CardViewForm_I.setOnClickListener(this);
         CardViewForm_II.setOnClickListener(this);
         CardViewForm_III.setOnClickListener(this);
-        CardViewForm_IV.setOnClickListener(this);
+
 
 
     }
@@ -40,11 +49,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent i;
         switch (v.getId()){
            case R.id.CardViewForm_I: i = new Intent(this,FormIActivity.class);startActivity(i);
-            CardViewForm_I.setCardBackgroundColor(Color.GREEN);
+           case R.id.CardViewForm_II: i = new Intent(this,FormIIActivity.class);startActivity(i);
 
 
-           case R.id.CardViewForm_III: i = new Intent(this,FormIIIActivity.class);startActivity(i);
-            default:break;
+           default:break;
+
         }
     }
 }

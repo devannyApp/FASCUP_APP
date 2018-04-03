50eba19ced3d4d7505fd5252f703aabad86c0acd
devannyApp <devannyApp@gmail.com>
2018-04-02 19:28:08 -0500

    Avances del 02 de Abril del 2018 hasta el formulario 2 terminado

--- app/src/main/java/android/fascup/com/fascup/FormIActivity.java
+++ app/src/main/java/android/fascup/com/fascup/FormIActivity.java
@@ -4,13 +4,50 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FormIActivity extends AppCompatActivity {
+
+    //Variables del Spinner Municipio
+    Spinner comboMunicipio;
+    Spinner comboReguardo;
+    Spinner comboVereda;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_i);
+
+        //Definimos Spinner municipio
+        comboMunicipio = findViewById(R.id.idSpinnerMunicipio);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.combo_municipio,android.R.layout.simple_spinner_item);
+        comboMunicipio.setAdapter(adapter);
+
+
+        //Definimos Spinner Reguardo
+        comboReguardo = findViewById(R.id.idSpinnerResguardo);
+        ArrayAdapter<CharSequence> adapterR = ArrayAdapter.createFromResource(this,R.array.combo_resguardo,android.R.layout.simple_spinner_item);
+        comboReguardo.setAdapter(adapterR);
+
+        //Definimos Spinner Vereda
+        comboVereda = findViewById(R.id.idSpinnerVereda);
+        ArrayAdapter<CharSequence> adapterV = ArrayAdapter.createFromResource(this,R.array.combo_vereda,android.R.layout.simple_spinner_item);
+        comboVereda.setAdapter(adapterV);
+        /*comboVereda.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                Toast.makeText(parent.getContext(), "Seleccionado: "+parent.getItemAtPosition(position).toString(),Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?>parent){
+
+            }
+        });*/
+
 
         //Muestra el boton para volver
         ActionBar actionBar = getSupportActionBar();
